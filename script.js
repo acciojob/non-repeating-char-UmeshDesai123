a15337c62b163723a832e045c614ce1a9d198fed
Umesh Desai <117746877+UmeshDesai123@users.noreply.github.com>
2024-01-23 10:26:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
+// your code here
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const map = {};
+	for(let i=0; i<str.length; i++){
+		if(map[str.charAt(i)] == undefined){
+            
+			
+            map[str.charAt(i)] = 1;
+            // console.log(map);
+		}
+		else{
+            // console.log('else')
+			map[str.charAt(i)] = map[str.charAt(i)]+1;
+		}
+	}
+    // console.log(map);
+	for( char in map){
+			if(map[char] == 1){
+				return char;
+			}
+		}
+	return null
 }
+// console.log(firstNonRepeatedChar('aadccb'))
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
